b91b3f8442e72b309ad475446ae04b9f215ab1a6
Thang Tran <thuutran@amperecomputing.com>
2022-09-22 09:36:15 +0700

    support handling boot sensor events
    Support handling boot progress code from the HOST. When HOST sends event
    data of sensors to BMC, BMC shall parse and log it to Redfish.
    
    Tested:
        1. Power cycle host.
        2. Check the Redfish log in below link.
           <BMCIP>redfish/v1/Systems/system/LogServices/EventLog/Entries
        3. The event messages of SECpro, Mpro, ATF, DDR are displayed.
    
    Signed-off-by: Thang Tran <thuutran@amperecomputing.com>

---
+++ requester/numeric_sensor_state.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "common/types.hpp"
+#include "common/utils.hpp"
+#include "event_hander_interface.hpp"
+
+#include <systemd/sd-journal.h>
+
+#include <sdeventplus/event.hpp>
+#include <sdeventplus/source/event.hpp>
+
+namespace pldm
+{
+namespace events
+{
+namespace sensors
+{
+
+class NumericSensorHanler
+{
+  public:
+    NumericSensorHanler();
+    virtual ~NumericSensorHanler() = default;
+    NumericSensorHanler(const NumericSensorHanler&) = default;
+    NumericSensorHanler& operator=(const NumericSensorHanler&) = default;
+    NumericSensorHanler(NumericSensorHanler&&) = default;
+    NumericSensorHanler& operator=(NumericSensorHanler&&) = default;
+
+  private:
+    void handleDbusEventSignalMatch();
+
+    void addJournalRecord(const std::string& message, std::uint8_t tid,
+                          std::uint16_t sensorId, std::uint8_t eventState,
+                          std::uint8_t preEventState, uint8_t sensorDataSize,
+                          uint32_t presentReading)
+    {
+        try
+        {
+            sd_journal_send("MESSAGE=%s", message.c_str(), "TID=%d", tid,
+                            "SENSOR_ID=%d", sensorId, "EVENT_STATE=%d",
+                            eventState, "PRE_EVENT_STATE=%d", preEventState,
+                            "SENSOR_DATA_SIZE=%d", sensorDataSize,
+                            "PRESENT_READING=%d", presentReading);
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "ERROR:\n" << e.what() << std::endl;
+        }
+    }
+}; // class NumericSensorHanler
+} // namespace sensors
+} // namespace events
+} // namespace pldm
